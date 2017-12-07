b1f0318bbc3e48df2e74d2d6494210ba29af014b
Zac Brown <zbrown@incisent.com>
2017-12-07 14:37:31 -0600

    This doesn't work with Chef 11 + Ohai, which mis-identifies amazon linux as some crazy version of rhel.

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,10 +21,11 @@ package 'lvm2'
 
 # Start+Enable the lvmetad service on RHEL7, it is required by default
 # but not automatically started
-if node['platform_family'] == 'rhel' && node['platform_version'].to_i >= 7 && !platform?('amazon')
-  service 'lvm2-lvmetad' do
-    action [:enable, :start]
-    provider Chef::Provider::Service::Systemd
-    only_if '/sbin/lvm dumpconfig global/use_lvmetad | grep use_lvmetad=1'
-  end
-end
+
+# if node['platform_family'] == 'rhel' && node['platform_version'].to_i >= 7 && !platform?('amazon')
+#   service 'lvm2-lvmetad' do
+#     action [:enable, :start]
+#     provider Chef::Provider::Service::Systemd
+#     only_if '/sbin/lvm dumpconfig global/use_lvmetad | grep use_lvmetad=1'
+#   end
+# end
